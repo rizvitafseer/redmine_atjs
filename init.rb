1feaf7c445b8ce6c69cd8a885e111a13e7f18ea9
Tafseer Rizvi <rizvi@xerago.com>
2022-07-25 11:44:32 +0530

    Redmine 5 update

--- init.rb
+++ init.rb
@@ -3,7 +3,9 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_atjs do
   name "Redmine AtJS Integration"
-  author 'Alex Dergachev'
+  author 'Rizvi'
+  url 'https://github.com/rizvitafseer/redmine_atjs.git'
+  author_url 'https://github.com/rizvitafseer/redmine_atjs.git'
   
   description 'Enable #issue autocompletion in issues and wikis'
   version '0.0.2'
